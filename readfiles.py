3db8d821916754a6f6f600b5f1bf05a8285f6b47
JURU <jurassu10@gmail.com>
2019-02-12 08:59:18 +0200

    add  the saving file

--- readfiles.py
+++ readfiles.py
@@ -2,7 +2,13 @@ text_file = "test.txt"
 def read_file(text_file):
     '''
     Function that reads a text file and returns the data from the text file
+    Raises:
+        FileNotFoundError:If it cannot the file
     '''
-    with open(text_file,"r") as handle:
-        data = handle.read()
-        return data
+    try:
+        with open(text_file,"r") as handle:
+            data = handle.read()
+            return data
+
+    except FileNotFoundError:
+        return None
